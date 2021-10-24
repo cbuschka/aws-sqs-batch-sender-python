29d217eb2b11d22acf5b2050392fce583ffcafb1
conni <cbuschka@gmail.com>
2021-10-24 22:28:22 +0200

    Added requirements and packages to setup.py.

--- setup.py
+++ setup.py
@@ -7,6 +7,13 @@ def get_readme():
         readme = fh.read()
         return readme
 
+
+def get_requirements():
+    with open(os.path.join(PACKAGE_ROOT, 'requirements.txt')) as f:
+        requirements =[r.strip() for r in f.readlines()]
+        return requirements
+
+
 def get_version():
     proc = subprocess.Popen(['git', 'describe', '--dirty', '--tags'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     stdout, stderr = proc.communicate()
@@ -15,23 +22,33 @@ def get_version():
         raise ValueError("Invalid version: '{}'.".format(result))
     return result.group(1)
 
+
+requirements = get_requirements()
 version = get_version()
 long_description = get_readme()
 
 setuptools.setup(
     name="aws-sqs-batch-sender",
     version=version,
+    install_requires=requirements,
     author="Cornelius Buschka",
     author_email="cbuschka@gmail.com",
     description="AWS SQS Batch Sender for boto3",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/cbuschka/aws-sqs-batch-sender-python",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=('tests',)),
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache Software License",
-        "Operating System :: OS Independent",
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Software Development',
     ],
     python_requires='>=3.6',
 )
